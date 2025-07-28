72057734c4784f211f9963e0cd8953aeab5a2f88
vlavoie <vincent.lavoie.cf@gmail.com>
2025-07-28 11:27:19 -0400

    remove flags from compile scripts

--- examples/cartridge/compile.sh
+++ examples/cartridge/compile.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)/../..
 
 mkdir -p build
 
-clang++ $FLAGS -std=c++14 -o build/cartridge_d -Iinclude -Wall -g \
-  examples/cartridge/main.cc                                      \
+clang++ -std=c++14 -o build/cartridge_d -Iinclude -Wall -g \
+  examples/cartridge/main.cc                               \
   include/cartridge.cc
--- examples/image/compile.sh
+++ examples/image/compile.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)/../..
 
 mkdir -p build
 
-clang++ $FLAGS -std=c++14 -o build/image_d -Iinclude -Wall -lX11 -lGL -g \
-  examples/image/main.cc                                                       \
+clang++ -std=c++14 -o build/image_d -Iinclude -Wall -lX11 -lGL -g \
+  examples/image/main.cc                                          \
   include/tga.cc
